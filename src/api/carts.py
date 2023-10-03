ce129c753217fca128149d75187754567e1aa5c7
misakitanabe <misaki.a.tanabe@gmail.com>
2023-10-02 21:34:47 -0700

    added carts

--- src/api/carts.py
+++ src/api/carts.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter, Depends, Request
 from pydantic import BaseModel
 from src.api import auth
+import sqlalchemy
+from src import database as db
 
 router = APIRouter(
     prefix="/carts",
@@ -12,11 +14,20 @@ router = APIRouter(
 class NewCart(BaseModel):
     customer: str
 
+carts = {}
+cart_counter = 0
 
 @router.post("/")
 def create_cart(new_cart: NewCart):
     """ """
-    return {"cart_id": 1}
+    # return {"cart_id": 1}
+    global cart_counter
+    global carts
+    
+    cart_counter += 1
+    carts[cart_counter] = []
+    return {"cart_id": cart_counter}
+
 
 
 @router.get("/{cart_id}")
@@ -33,6 +44,15 @@ class CartItem(BaseModel):
 @router.post("/{cart_id}/items/{item_sku}")
 def set_item_quantity(cart_id: int, item_sku: str, cart_item: CartItem):
     """ """
+    global cart_counter
+    global carts
+
+    for order in carts[cart_id]:
+        if order[0] == item_sku:
+            order[1] = cart_item.quantity
+            return "OK"
+
+    carts[cart_id].append([item_sku, cart_item.quantity])
 
     return "OK"
 
@@ -43,5 +63,25 @@ class CartCheckout(BaseModel):
 @router.post("/{cart_id}/checkout")
 def checkout(cart_id: int, cart_checkout: CartCheckout):
     """ """
+    global cart_counter
+    global carts
 
-    return {"total_potions_bought": 1, "total_gold_paid": 50}
+    print("cart_checkout.payment = ", cart_checkout.payment)
+
+    with db.engine.begin() as connection:
+        row = connection.execute(sqlalchemy.text("SELECT * FROM global_inventory")).fetchone()
+        num_red_potions = row[0] 
+        gold = row[2]
+        purchased = carts[cart_id][0][1]
+
+        # only sell to customer if enough in catalog
+        if purchased <= num_red_potions:
+            num_red_potions -= purchased 
+            gold += (purchased * 50)
+
+            connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_red_potions = {}".format(num_red_potions)))
+            connection.execute(sqlalchemy.text("UPDATE global_inventory SET gold = {}".format(gold)))
+
+            return {"total_potions_bought": purchased, "total_gold_paid": 50 * purchased}
+    
+    return {"total_potions_bought": 0, "total_gold_paid": 0}
