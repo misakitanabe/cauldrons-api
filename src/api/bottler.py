58c1bee3c63cf1d4fe7324b5cc326523cd6be89e
misakitanabe <misaki.a.tanabe@gmail.com>
2023-10-02 21:34:35 -0700

    added bottler

--- src/api/bottler.py
+++ src/api/bottler.py
@@ -2,6 +2,8 @@ from fastapi import APIRouter, Depends
 from enum import Enum
 from pydantic import BaseModel
 from src.api import auth
+import sqlalchemy
+from src import database as db
 
 router = APIRouter(
     prefix="/bottler",
@@ -18,6 +20,16 @@ def post_deliver_bottles(potions_delivered: list[PotionInventory]):
     """ """
     print(potions_delivered)
 
+    with db.engine.begin() as connection:
+        row = connection.execute(sqlalchemy.text("SELECT * FROM global_inventory")).fetchone()
+        num_red_potions = row[0]
+
+        for potion in potions_delivered:
+            if potion.potion_type == [1, 0, 0, 0]:
+                num_red_potions += potion.quantity
+
+        connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_red_potions = {}".format(num_red_potions)))
+
     return "OK"
 
 # Gets called 4 times a day
@@ -32,10 +44,19 @@ def get_bottle_plan():
     # Expressed in integers from 1 to 100 that must sum up to 100.
 
     # Initial logic: bottle all barrels into red potions.
+    with db.engine.begin() as connection:
+        row = connection.execute(sqlalchemy.text("SELECT * FROM global_inventory")).fetchone()
+        num_red_ml = row[1]
+        count = 0
+
+        while num_red_ml >= 100:
+            num_red_ml -= 100
+            connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_red_ml = {}".format(num_red_ml)))
+            count += 1
 
     return [
             {
                 "potion_type": [100, 0, 0, 0],
-                "quantity": 5,
+                "quantity": count,
             }
         ]
