45daf91f4d554ad75d5cb7b1ccc9b27516c76567
misakitanabe <misaki.a.tanabe@gmail.com>
2023-10-08 19:50:10 -0700

    Updated bottler to include red, green, blue

--- src/api/bottler.py
+++ src/api/bottler.py
@@ -21,19 +21,31 @@ def post_deliver_bottles(potions_delivered: list[PotionInventory]):
     print(potions_delivered)
 
     with db.engine.begin() as connection:
-        row = connection.execute(sqlalchemy.text("SELECT * FROM global_inventory")).fetchone()
-        num_red_potions = row[0]
-        num_red_ml = row[1]
+        row = connection.execute(sqlalchemy.text("SELECT num_red_potions, num_green_potions, num_blue_potions, num_red_ml, num_green_ml, num_blue_ml FROM global_inventory")).fetchone()
+        if row is not None:
+            num_red_potions = row[0]
+            num_green_potions = row[1]
+            num_blue_potions = row[2]
+            num_red_ml = row[3]
+            num_green_ml = row[4]
+            num_blue_ml = row[5]
+        print("Bottlers delivered BEFORE: red:", num_red_potions, "green:", num_green_potions, "blue:", num_blue_potions)
 
         for potion in potions_delivered:
-            if potion.potion_type == [1, 0, 0, 0]:
+            if potion.potion_type == [100, 0, 0, 0]:
                 num_red_potions += potion.quantity
                 num_red_ml -= (potion.quantity * 100)
+            elif potion.potion_type == [0, 100, 0, 0]:
+                num_green_potions += potion.quantity
+                num_green_ml -= (potion.quantity * 100)
+            elif potion.potion_type == [0, 0, 100, 0]:
+                num_blue_potions += potion.quantity
+                num_blue_ml -= (potion.quantity * 100)
 
-        print("Bottlers delivered: potion increase to ", num_red_potions, "ml decrease to ", num_red_ml)
+        print("Bottlers delivered AFTER: red:", num_red_potions, "green:", num_green_potions, "blue:", num_blue_potions)
 
-        connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_red_potions = {}".format(num_red_potions)))
-        connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_red_ml = {}".format(num_red_ml)))
+        connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_red_potions = {}, num_green_potions = {}, num_blue_potions = {}".format(num_red_potions, num_green_potions, num_blue_potions)))
+        connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_red_ml = {}, num_green_ml = {}, num_blue_ml = {}".format(num_red_ml, num_green_ml, num_blue_ml)))
 
     return "OK"
 
@@ -50,18 +62,41 @@ def get_bottle_plan():
 
     # Initial logic: bottle all barrels into red potions.
     with db.engine.begin() as connection:
-        row = connection.execute(sqlalchemy.text("SELECT * FROM global_inventory")).fetchone()
-        num_red_ml = row[1]
-        count = 0
+        row = connection.execute(sqlalchemy.text("SELECT num_red_ml, num_green_ml, num_blue_ml, gold FROM global_inventory")).fetchone()
+        if row is not None:
+            num_red_ml = row[0]
+            num_green_ml = row[1]
+            num_blue_ml = row[2]
+            gold = row[3]
 
-        while num_red_ml >= 100:
-            num_red_ml -= 100
-            # connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_red_ml = {}".format(num_red_ml)))
-            count += 1
+        red_count = int(num_red_ml / 100)
+        green_count = int(num_green_ml / 100)
+        blue_count = int(num_blue_ml / 100)
 
-    return [
-            {
-                "potion_type": [100, 0, 0, 0],
-                "quantity": count,
-            }
-        ]
+        # Append potions to purchase to plan if planning to buy one or more
+        plan = []
+        if red_count > 0:
+            plan.append(
+                {
+                    "potion_type": [100, 0, 0, 0],
+                    "quantity": red_count,
+                }
+            )
+        if green_count > 0:
+            plan.append(
+                {
+                    "potion_type": [0, 100, 0, 0],
+                    "quantity": green_count,
+                }
+            )
+        if blue_count > 0:
+            plan.append(
+                {
+                    "potion_type": [0, 0, 100, 0],
+                    "quantity": blue_count,
+                }
+            )
+
+    print("Bottlers Plan:", plan)
+
+    return plan
