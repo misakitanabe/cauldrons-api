d1e0944763cb65edcdab0dda0da9ef49094e4279
misakitanabe <misaki.a.tanabe@gmail.com>
2023-10-08 19:46:32 -0700

    Updated carts to be able to buy red, green, and blue

--- src/api/carts.py
+++ src/api/carts.py
@@ -20,7 +20,6 @@ cart_counter = 0
 @router.post("/")
 def create_cart(new_cart: NewCart):
     """ """
-    # return {"cart_id": 1}
     global cart_counter
     global carts
     
@@ -47,13 +46,18 @@ def set_item_quantity(cart_id: int, item_sku: str, cart_item: CartItem):
     global cart_counter
     global carts
 
+    print("Carts set_item_quantity:", cart_item, "for", item_sku, "cart-id:", cart_id)
+
+    # if item to set the quantity of already exists in cart, update the quantity to new quantity in cart_item
     for order in carts[cart_id]:
         if order[0] == item_sku:
             order[1] = cart_item.quantity
+            print("Succesfully updated quantity of item")
             return "OK"
 
+    # else, adds that item to cart with quantity in cart_item
     carts[cart_id].append([item_sku, cart_item.quantity])
-
+    print("Succesfully added item to cart")
     return "OK"
 
 
@@ -69,25 +73,50 @@ def checkout(cart_id: int, cart_checkout: CartCheckout):
     print("cart_checkout.payment = ", cart_checkout.payment)
 
     with db.engine.begin() as connection:
-        row = connection.execute(sqlalchemy.text("SELECT * FROM global_inventory")).fetchone()
-        num_red_potions = row[0] 
-        gold = row[2]
-        purchased = carts[cart_id][0][1]
-        print("carts: customer wants ", purchased, "potions and i have ", num_red_potions, "potions")
+        row = connection.execute(sqlalchemy.text("SELECT num_red_potions, num_green_potions, num_blue_potions, gold FROM global_inventory")).fetchone()
+        if row is not None:
+            num_red_potions = row[0]
+            num_green_potions = row[1]
+            num_blue_potions = row[2]
+            gold = row[3]
 
-        # only sell to customer if enough in catalog
-        if purchased <= num_red_potions:
-            num_red_potions -= purchased 
-            gold += (purchased * 50)
+        print("cart checkout BEFORE: red:", num_red_potions, "green:", num_green_potions, "blue:", num_blue_potions, "gold:", gold)
 
-            connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_red_potions = {}".format(num_red_potions)))
-            connection.execute(sqlalchemy.text("UPDATE global_inventory SET gold = {}".format(gold)))
+        # update potion count and gold for every item in their cart
+        for item in carts[cart_id]:
+            if item[0] == "RED_POTION_0":
+                print("carts checkout: customer wants ", item[1], item[0], "potions and i have ", num_red_potions, "potions")
+                
+                # only sell to customer if enough in catalog
+                if item[1] <= num_red_potions:
+                    num_red_potions -= item[1]
+                    gold += (item[1] * 50) 
+                    print("Success selling", item[1], "red potions")
+            
+            elif item[0] == "GREEN_POTION_0":
+                print("carts checkout: customer wants ", item[1], item[0], "potions and i have ", num_green_potions, "potions")
+                
+                # only sell to customer if enough in catalog
+                if item[1] <= num_green_potions:
+                    num_green_potions -= item[1]
+                    gold += (item[1] * 50) 
+                    print("Success selling", item[1], "green potions")
 
-            # remove cart from carts
-            carts.pop(cart_id) 
+            elif item[0] == "BLUE_POTION_0":
+                print("carts checkout: customer wants ", item[1], item[0], "potions and i have ", num_blue_potions, "potions")
+                
+                # only sell to customer if enough in catalog
+                if item[1] <= num_blue_potions:
+                    num_green_potions -= item[1]
+                    gold += (item[1] * 60) 
+                    print("Success selling", item[1], "blue potions")
 
-            print("purchase success")
-            return {"total_potions_bought": purchased, "total_gold_paid": 50 * purchased}
-    
-    print("none purchased")
-    return {"total_potions_bought": 0, "total_gold_paid": 0}
+        # update database 
+        connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_red_potions = {}, num_green_potions = {}, num_blue_potions = {}, gold = {}".format(num_red_potions, num_green_potions, num_blue_potions, gold)))
+
+    # remove cart from carts
+    carts.pop(cart_id) 
+
+    print("cart checkout AFTER: red:", num_red_potions, "green:", num_green_potions, "blue:", num_blue_potions, "gold:", gold)
+
+    return {"cart checkout AFTER: red:", num_red_potions, "green:", num_green_potions, "blue:", num_blue_potions, "gold:", gold}
