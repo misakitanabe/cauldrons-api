fa695d1c81fa2b6355bd8f25271ad9c4deeb2cff
misakitanabe <misaki.a.tanabe@gmail.com>
2023-10-15 16:34:07 -0700

    added dark ml in bottlers deliver

--- src/api/barrels.py
+++ src/api/barrels.py
@@ -26,14 +26,15 @@ def post_deliver_barrels(barrels_delivered: list[Barrel]):
     print(barrels_delivered)
 
     with db.engine.begin() as connection:
-        row = connection.execute(sqlalchemy.text("SELECT num_red_ml, num_green_ml, num_blue_ml, gold FROM global_inventory")).fetchone()
+        row = connection.execute(sqlalchemy.text("SELECT num_red_ml, num_green_ml, num_blue_ml, num_dark_ml, gold FROM global_inventory")).fetchone()
         if row is not None:
             num_red_ml = row[0]
             num_green_ml = row[1]
             num_blue_ml = row[2]
-            gold = row[3]
+            num_dark_ml = row[3]
+            gold = row[4]
 
-        print("Barrels delivered BEFORE: red ml:", num_red_ml, "green:", num_green_ml, "blue:", num_blue_ml, "gold:", gold)
+        print("Barrels delivered BEFORE: red ml:", num_red_ml, "green:", num_green_ml, "blue:", num_blue_ml, "dark:", num_dark_ml, "gold:", gold)
 
         # add ml and pay gold for every barrel delivered 
         for barrel in barrels_delivered:
@@ -43,12 +44,14 @@ def post_deliver_barrels(barrels_delivered: list[Barrel]):
                 num_green_ml += barrel.ml_per_barrel * barrel.quantity
             elif barrel.potion_type == [0, 0, 1, 0]:
                 num_blue_ml += barrel.ml_per_barrel * barrel.quantity
+            else:
+                num_dark_ml += barrel.ml_per_barrel * barrel.quantity
             gold -= barrel.price 
 
         print("Barrels delivered AFTER: red ml:", num_red_ml, "green:", num_green_ml, "blue:", num_blue_ml, "gold:", gold)
         
-        connection.execute(sqlalchemy.text("UPDATE global_inventory SET gold = {}, num_red_ml = {}, num_green_ml = {}, num_blue_ml = {}".format(gold, num_red_ml, num_green_ml, num_blue_ml)))
-        
+        connection.execute(sqlalchemy.text("UPDATE global_inventory SET gold = {}, num_red_ml = {}, num_green_ml = {}, num_blue_ml = {}, num_dark_ml = {}".format(gold, num_red_ml, num_green_ml, num_blue_ml, num_dark_ml)))
+
     return "OK"
 
 # Gets called once a day
