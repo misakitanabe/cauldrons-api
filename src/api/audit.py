89a6a8f9b043d55de5c4c0cd4ffa924d72546dee
misakitanabe <misaki.a.tanabe@gmail.com>
2023-10-16 11:30:16 -0700

    implemented audit

--- src/api/audit.py
+++ src/api/audit.py
@@ -2,6 +2,8 @@ from fastapi import APIRouter, Depends
 from pydantic import BaseModel
 from src.api import auth
 import math
+import sqlalchemy
+from src import database as db
 
 router = APIRouter(
     prefix="/audit",
@@ -12,8 +14,17 @@ router = APIRouter(
 @router.get("/inventory")
 def get_inventory():
     """ """
+    with db.engine.begin() as connection:
+        globals = connection.execute(sqlalchemy.text("SELECT(num_red_ml + num_green_ml + num_blue_ml + num_dark_ml) AS total_ml, gold FROM global_inventory")).fetchone()
+        num_potions = connection.execute(sqlalchemy.text("SELECT SUM(quantity) as total_quantity FROM potions")).scalar_one()
+
+    if globals is not None:
+        mls = globals[0]
+        gold = globals[1]
+
+    print("AUDIT: number_of_potions", num_potions, "ml_in_barrels", mls, "gold", gold)
     
-    return {"number_of_potions": 0, "ml_in_barrels": 0, "gold": 0}
+    return {"number_of_potions": num_potions, "ml_in_barrels": mls, "gold": gold}
 
 class Result(BaseModel):
     gold_match: bool
