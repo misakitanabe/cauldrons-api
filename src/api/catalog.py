9beb8e8d025d22dc78d70f1e2f92a490165c6efa
misakitanabe <misaki.a.tanabe@gmail.com>
2023-10-23 13:31:13 -0700

    fixed query in catalog to not reference quantity anymore

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -19,18 +19,18 @@ def get_catalog():
                                                     SELECT p.sku, p.name, SUM(trans.change) AS quantity, p.price, p.potion_type 
                                                     FROM potions AS p 
                                                     LEFT JOIN potion_ledger_entries AS trans ON p.id = trans.potion_id
-                                                    WHERE quantity > 0
                                                     GROUP BY p.id
                                                     """)).fetchall()
 
         for row in potions:
-            catalog.append({
-                "sku": row[0],
-                "name": row[1],
-                "quantity": row[2],
-                "price": row[3],
-                "potion_type": row[4],
-            })
+            if row[2] is not None and row[2] is not 0:
+                catalog.append({
+                    "sku": row[0],
+                    "name": row[1],
+                    "quantity": row[2],
+                    "price": row[3],
+                    "potion_type": row[4],
+                })
             
     return catalog
 
