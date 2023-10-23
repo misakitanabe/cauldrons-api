efa43e884b894bbd782b3c3eaa74566ee6303a03
misakitanabe <misaki.a.tanabe@gmail.com>
2023-10-22 19:51:09 -0700

    Updated barrels to use ledger schemas instead

--- src/api/barrels.py
+++ src/api/barrels.py
@@ -26,33 +26,99 @@ def post_deliver_barrels(barrels_delivered: list[Barrel]):
     print(barrels_delivered)
 
     with db.engine.begin() as connection:
-        row = connection.execute(sqlalchemy.text("SELECT num_red_ml, num_green_ml, num_blue_ml, num_dark_ml, gold FROM global_inventory")).fetchone()
-        if row is not None:
-            num_red_ml = row[0]
-            num_green_ml = row[1]
-            num_blue_ml = row[2]
-            num_dark_ml = row[3]
-            gold = row[4]
+        # queries here just for debugging not actually used in updates
+        gold = connection.execute(sqlalchemy.text("SELECT SUM(change) FROM gold_ledger_entries")).scalar_one()
+        ml = connection.execute(sqlalchemy.text("""
+                                                SELECT SUM(change) 
+                                                FROM ml_ledger_entries
+                                                GROUP BY type
+                                                ORDER BY type ASC
+                                                """)).fetchall()
+        if ml is not None:
+            num_blue_ml = ml[0][0]
+            num_dark_ml = ml[1][0]
+            num_green_ml = ml[2][0]
+            num_red_ml = ml[3][0]
 
         print("Barrels delivered BEFORE: red ml:", num_red_ml, "green:", num_green_ml, "blue:", num_blue_ml, "dark:", num_dark_ml, "gold:", gold)
+
+        red_diff = 0
+        green_diff = 0
+        blue_diff = 0
+        dark_diff = 0
+        gold_diff = 0
 
         # add ml and pay gold for every barrel delivered 
         for barrel in barrels_delivered:
             if barrel.potion_type == [1, 0, 0, 0]:
-                num_red_ml += barrel.ml_per_barrel * barrel.quantity
+                red_diff += barrel.ml_per_barrel * barrel.quantity
             elif barrel.potion_type == [0, 1, 0, 0]:
-                num_green_ml += barrel.ml_per_barrel * barrel.quantity
+                green_diff += barrel.ml_per_barrel * barrel.quantity
             elif barrel.potion_type == [0, 0, 1, 0]:
-                num_blue_ml += barrel.ml_per_barrel * barrel.quantity
+                blue_diff += barrel.ml_per_barrel * barrel.quantity
             else:
-                num_dark_ml += barrel.ml_per_barrel * barrel.quantity
-            gold -= barrel.price 
+                dark_diff += barrel.ml_per_barrel * barrel.quantity
+            gold_diff -= barrel.price 
 
-        print("Barrels delivered AFTER: red ml:", num_red_ml, "green:", num_green_ml, "blue:", num_blue_ml, "gold:", gold)
-        
-        connection.execute(sqlalchemy.text("UPDATE global_inventory SET gold = {}, num_red_ml = {}, num_green_ml = {}, num_blue_ml = {}, num_dark_ml = {}".format(gold, num_red_ml, num_green_ml, num_blue_ml, num_dark_ml)))
+        print("Barrels delivered AFTER: red ml:", num_red_ml + red_diff, "green:", num_green_ml + green_diff, "blue:", num_blue_ml + blue_diff, "gold:", gold + gold_diff)
+       
+        # update gold and ml's only if i paid any gold
+        if gold_diff < 0:
+            description = ('Paid barreler ' + str(gold_diff*-1) + ' gold for '
+                            + str(red_diff) + ' red mls, ' 
+                            + str(green_diff) + ' green mls, '
+                            + str(blue_diff) + ' blue mls, '
+                            + str(dark_diff) + ' dark mls')
+            print(description)
+            # add a new row into barrel_transactions schema
+            trans_id = insert_barrel_trans(description)
+
+            # update gold by adding row into gold_ledger_entries
+            connection.execute(
+                sqlalchemy.text("""
+                                INSERT INTO gold_ledger_entries (trans_id, change)
+                                VALUES
+                                (:trans_id, :gold_diff)
+                                """),
+                [{"trans_id": trans_id, "gold_diff" : gold_diff}])
+
+            # update ml by adding row into ml_ledger_entries
+            if red_diff > 0:
+                insert_ml_entry(trans_id, 'RED', red_diff)
+
+            if green_diff > 0:
+                insert_ml_entry(trans_id, 'GREEN', green_diff)
+
+            if blue_diff > 0:
+                insert_ml_entry(trans_id, 'BLUE', blue_diff)
+
+            if dark_diff > 0:
+                insert_ml_entry(trans_id, 'DARK', dark_diff)
 
     return "OK"
+
+def insert_barrel_trans(description):
+    with db.engine.begin() as connection:
+        trans_id = connection.execute(
+                sqlalchemy.text("""
+                                INSERT INTO barrel_transactions (description) 
+                                VALUES (:description)
+                                RETURNING id
+                                """),
+                [{"description": description}]).scalar_one()
+        
+    return trans_id
+
+def insert_ml_entry(trans_id, ml_type, diff):
+    with db.engine.begin() as connection:
+        connection.execute(
+                sqlalchemy.text("""
+                                INSERT INTO ml_ledger_entries (trans_id, type, change)
+                                VALUES
+                                (:trans_id, :ml_type, :diff)
+                                """),
+                [{"trans_id": trans_id, "ml_type" : ml_type, "diff" : diff}])
+
 
 # Gets called once a day
 @router.post("/plan")
@@ -63,8 +129,14 @@ def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
     plan = []
     
     with db.engine.begin() as connection:
-        gold = connection.execute(sqlalchemy.text("SELECT gold FROM global_inventory")).scalar_one()
-        potion_row = connection.execute(sqlalchemy.text("SELECT potion_type FROM potions ORDER BY quantity ASC")).fetchone()
+        gold = connection.execute(sqlalchemy.text("SELECT SUM(change) FROM gold_ledger_entries")).scalar_one()
+        potion_row = connection.execute(sqlalchemy.text("""
+                                                        SELECT potion_type 
+                                                        FROM potions AS p 
+                                                        LEFT JOIN potion_ledger_entries AS entries ON p.id = entries.potion_id
+                                                        GROUP BY p.id
+                                                        ORDER BY SUM(entries.change) ASC
+                                                        """)).fetchone()
 
         if potion_row is not None:
             least_type = potion_row[0]
@@ -77,21 +149,27 @@ def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
                 least = "MINI_BLUE_BARREL"
             else:
                 least = "SMALL_GREEN_BARREL"
+
+        if gold is None:
+            gold = 0
             
 
         print("Barrels Plan: Trying to buy", least)
 
     # purchase one small barrel if i can afford it
     for barrel in wholesale_catalog:
-        if barrel.price < gold and barrel.sku == least:
-            print("Successfully added to plan:", barrel.sku)
-            return [
-                {
-                    "sku": barrel.sku,
-                    "quantity": 1,
-                }
-            ]
-     
+        if barrel.sku == least:
+            if barrel.price <= gold:
+                print("Successfully added to plan:", barrel.sku)
+                plan.append(
+                    {
+                        "sku": barrel.sku,
+                        "quantity": 1,
+                    }
+                )
+            else:
+                print("Not enough gold:", gold)
+                least = "SMALL_RED_BARREL"
     
     print("Barrels plan:", plan, "gold:", gold)
     return plan
