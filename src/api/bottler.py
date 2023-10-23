5fc32c23f0fb8e21ba9ce3c0e8a38b6c0e8bd26e
misakitanabe <misaki.a.tanabe@gmail.com>
2023-10-23 01:29:08 -0700

    updated bottler deliver: got rid of transactions table

--- src/api/bottler.py
+++ src/api/bottler.py
@@ -21,52 +21,88 @@ def post_deliver_bottles(potions_delivered: list[PotionInventory]):
     print(potions_delivered)
 
     with db.engine.begin() as connection:
-        row = connection.execute(sqlalchemy.text("SELECT num_red_ml, num_green_ml, num_blue_ml, num_dark_ml FROM global_inventory")).fetchone()
-
-        if row is not None:
-            num_red_ml = row[0]
-            num_green_ml = row[1]
-            num_blue_ml = row[2]
-            num_dark_ml = row[3]
-
-        # print how many potions
-        potions = connection.execute(sqlalchemy.text("SELECT sku, quantity FROM potions")).fetchall()
+        # queries here just for debugging not actually used in updates
+        mls = connection.execute(
+            sqlalchemy.text("""
+                            SELECT SUM(change) 
+                            FROM ml_ledger_entries
+                            GROUP BY type
+                            ORDER BY type ASC
+                            """)
+            ).fetchall()
+        
+        if mls is not None:
+            num_blue_ml = mls[0][0]
+            num_dark_ml = mls[1][0]
+            num_green_ml = mls[2][0]
+            num_red_ml = mls[3][0]
+        
         print("Bottlers delivered BEFORE currently have red ml:", num_red_ml, "green:", num_green_ml, "blue:", num_blue_ml)
-        for potion in potions:
-            print(potion[0], potion[1])
+
+        red_diff = 0
+        green_diff = 0
+        blue_diff = 0
+        dark_diff = 0
 
         for potion in potions_delivered:
-            num_red_ml -= potion.potion_type[0] * potion.quantity
-            num_green_ml -= potion.potion_type[1] * potion.quantity
-            num_blue_ml -= potion.potion_type[2] * potion.quantity
-            num_dark_ml -= potion.potion_type[3] * potion.quantity
-            print("Bottlers delivered:", potion.quantity, potion.potion_type)
-            connection.execute(
-                sqlalchemy.text("""
-                                UPDATE potions 
-                                SET quantity = quantity + :additional_quantity
-                                WHERE potion_type = :potion_type
-                                """),
-                [{"additional_quantity": potion.quantity, "potion_type": potion.potion_type}])
+            red_diff -= potion.potion_type[0] * potion.quantity
+            green_diff -= potion.potion_type[1] * potion.quantity
+            blue_diff -= potion.potion_type[2] * potion.quantity
+            dark_diff -= potion.potion_type[3] * potion.quantity
 
-        connection.execute(
-            sqlalchemy.text("""
-                            UPDATE global_inventory SET 
-                            num_red_ml = :num_red_ml, 
-                            num_green_ml = :num_green_ml, 
-                            num_blue_ml = :num_blue_ml,
-                            num_dark_ml = :num_dark_ml
-                            """),
-            [{"num_red_ml": num_red_ml, "num_green_ml": num_green_ml, "num_blue_ml": num_blue_ml, "num_dark_ml": num_dark_ml}])
+            # create new row in potions table for each potion delivered
+            description = ('Received ' + str(potion.quantity) + str(potion.potion_type) + 'potions from bottler.')
+            print(description)
+            insert_potion_entry(description, potion.quantity, potion.potion_type)
+            
+        # create new transaction and row in ml table for total ml's used
+        description = ('Paid bottler ' 
+                        + str(red_diff) + ' red mls, ' 
+                        + str(green_diff) + ' green mls, '
+                        + str(blue_diff) + ' blue mls, '
+                        + str(dark_diff) + ' dark mls')
+        print(description)
+
+        # update ml by adding row into ml_ledger_entries
+        if red_diff < 0:
+            insert_ml_entry(description, 'RED', red_diff)
+
+        if green_diff < 0:
+            insert_ml_entry(description, 'GREEN', green_diff)
+
+        if blue_diff < 0:
+            insert_ml_entry(description, 'BLUE', blue_diff)
+
+        if dark_diff < 0:
+            insert_ml_entry(description, 'DARK', dark_diff)
+
+      
+        print("Bottlers delivered AFTER currently have red ml:", num_red_ml + red_diff, "green:", num_green_ml + green_diff, "blue:", num_blue_ml + blue_diff)
         
-        # print how many potionss
-        potions = connection.execute(sqlalchemy.text("SELECT sku, quantity FROM potions")).fetchall()
-        print("Bottlers delivered AFTER currently have red ml:", num_red_ml, "green:", num_green_ml, "blue:", num_blue_ml)
-        for potion in potions:
-            print(potion[0], potion[1])
 
     return "OK"
 
+
+def insert_potion_entry(description, additional_quantity, potion_type):
+    with db.engine.begin() as connection:
+        connection.execute(
+                sqlalchemy.text("""
+                                INSERT INTO potion_ledger_entries (description, potion_id, change)
+                                VALUES
+                                (:description, (SELECT id FROM potions WHERE potion_type = :potion_type), :additional_quantity)
+                                """),
+                [{"description" : description, "additional_quantity": additional_quantity, "potion_type": potion_type}])
+        
+        
+def insert_ml_entry(description, ml_type, diff):
+    with db.engine.begin() as connection:
+        connection.execute(
+                sqlalchemy.text("""
+                                INSERT INTO ml_ledger_entries (description, type, change)
+                                VALUES
+                                (:description, :ml_type, :diff)
+                                """),
+                [{"description": description, "ml_type" : ml_type, "diff" : diff}])
    
 
 # Gets called 4 times a day
@@ -75,80 +111,62 @@ def get_bottle_plan():
     """
     Go from barrel to bottle.
     """
-
-    # Each bottle has a quantity of what proportion of red, blue, and
-    # green potion to add.
-    # Expressed in integers from 1 to 100 that must sum up to 100.
-
-    # Initial logic: bottle all barrels into red potions.
-    with db.engine.begin() as connection:
-        row = connection.execute(sqlalchemy.text("SELECT num_red_ml, num_green_ml, num_blue_ml, num_dark_ml FROM global_inventory")).fetchone()
-        if row is not None:
-            num_red_ml = row[0]
-            num_green_ml = row[1]
-            num_blue_ml = row[2]
-            num_dark_ml = row[3]
-
-        potions = connection.execute(sqlalchemy.text("SELECT quantity, potion_type FROM potions ORDER BY quantity ASC")).fetchall()
-        
     plan = []
+
+    with db.engine.begin() as connection:
+        # gets number of mls
+        mls = connection.execute(
+            sqlalchemy.text("""
+                            SELECT SUM(change) 
+                            FROM ml_ledger_entries
+                            GROUP BY type
+                            ORDER BY type ASC
+                            """)
+            ).fetchall()
+        
+        if mls is not None:
+            num_blue_ml = mls[0][0]
+            num_dark_ml = mls[1][0]
+            num_green_ml = mls[2][0]
+            num_red_ml = mls[3][0]
+
+        # gets potion types starting with lowest inventory to high
+        potions = connection.execute(
+            sqlalchemy.text("""
+                            SELECT SUM(entries.change) AS quantity, p.potion_type 
+                            FROM potions AS p 
+                            LEFT JOIN potion_ledger_entries AS entries ON p.id = entries.potion_id
+                            GROUP BY p.id
+                            ORDER BY quantity ASC
+                            """)
+            ).fetchall()
 
     print("Bottlers Plan currently have red ml:", num_red_ml, "green:", num_green_ml, "blue:", num_blue_ml)
 
-    # hardcode purple for now for assignment3
-    quantity = 0
-    while num_red_ml >= 50 and num_blue_ml >= 50:
-        quantity += 1
-        num_red_ml -= 50
-        num_blue_ml -= 50
-
-    if quantity > 0:
-        plan.append (
-            {
-                "potion_type": [50, 0, 50, 0],
-                "quantity": quantity
-            }
-        )
-
-    # hardcode teal for now for assignment3
-    quantity = 0
-    while num_green_ml >= 50 and num_blue_ml >= 50:
-        quantity += 1
-        num_green_ml -= 50
-        num_blue_ml -= 50
-
-    if quantity > 0:
-        plan.append (
-            {
-                "potion_type": [0, 50, 50, 0],
-                "quantity": quantity
-            }
-        )
-
     # creates bottle plan for each potion starting from ones with lowest stock, as they were ordered ascendingly by quantity
-    # for potion in potions:
-    #     quantity = 0
-    #     potion_type = potion[1]
-    #     # gets the recipe of each potion, and ml needed of each type for that potion
-    #     needed_red = potion_type[0]
-    #     needed_green = potion_type[1]
-    #     needed_blue = potion_type[2]
-    #     needed_dark = potion_type[3]
+    for potion in potions:
+        quantity = 0
+        potion_type = potion[1]
+        # gets the recipe of each potion, and ml needed of each type for that potion
+        needed_red = potion_type[0]
+        needed_green = potion_type[1]
+        needed_blue = potion_type[2]
+        needed_dark = potion_type[3]
 
-    #     while num_red_ml >= needed_red and num_green_ml >= needed_green and num_blue_ml >= needed_blue and num_dark_ml >= needed_dark:
-    #         quantity += 1
-    #         num_red_ml -= needed_red
-    #         num_green_ml -= needed_green
-    #         num_blue_ml -= needed_blue
-    #         num_dark_ml -= needed_dark
+        while num_red_ml >= needed_red and num_green_ml >= needed_green and num_blue_ml >= needed_blue and num_dark_ml >= needed_dark:
+            quantity += 1
+            num_red_ml -= needed_red
+            num_green_ml -= needed_green
+            num_blue_ml -= needed_blue
+            num_dark_ml -= needed_dark
         
-    #     if quantity > 0:
-    #         plan.append (
-    #             {
-    #                 "potion_type": potion_type,
-    #                 "quantity": quantity
-    #             }
-    #         )
+        if quantity > 0:
+            plan.append (
+                {
+                    "potion_type": potion_type,
+                    "quantity": quantity
+                }
+            )
     
     print("Bottlers Plan:", plan)
 
